ba296b08dc2a002d5000409e513e5d11ecc3f9cb
nabbae <shafirahnabilah1@gmail.com>
2025-07-02 14:19:07 +0700

    Add files via upload

---
+++ karakter1.py
@@ -0,0 +1,13 @@
+class player1:
+    def __init__(self,nama,hp,pow):
+        self.nama = nama ##label
+        self.hp = hp ##progress bar
+        self.pow = pow
+
+player1_list = [
+    player1("Tigreal", 10000, 150),
+    player1("Layla", 5000, 500),
+    player1("Nana", 7500, 250),
+    player1("Argus", 7500, 350),
+    player1("Eudora", 5000, 550)
+]
